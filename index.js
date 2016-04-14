84dbcac7eb3389d76f4cde6e209110d8386f98d8
zhaobing0908 <924540776@qq.com>
2016-04-14 10:37:47 +0800

    updata

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var winW = document.documentElement.clientWidth;
 var winH = document.documentElement.clientHeight;
 
 var desW = 640;
-var desH = 1136;
+var desH = 960;
 if (winW / winH <= desW / desH) {
     main.style.webkitTransform = "scale(" + winH / desH + ")";
 } else {
