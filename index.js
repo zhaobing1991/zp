ee288745e77fff51807728cc038c3aa08ac87162
zhaobing0908 <924540776@qq.com>
2016-04-14 13:22:27 +0800

    5

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var winW = document.documentElement.clientWidth;
 var winH = document.documentElement.clientHeight;
 
 var desW = 640;
-var desH = 1136;
+var desH = 960;
 if(desW/desH<winW/winH){
     main.style.webkitTransform = 'scale('+winW/desW+')';
 }else{
