55915a18a59c580fb2e82082adb377949999da3b
zhaobing0908 <924540776@qq.com>
2016-04-14 09:57:51 +0800

    ““原始”

---
+++ index.js
@@ -0,0 +1,69 @@
+/**
+ * Created by c1 on 2016/4/11.
+ */
+var main = document.querySelector("#box");
+var oLis = document.querySelectorAll("#ul1>li");
+var winW = document.documentElement.clientWidth;
+var winH = document.documentElement.clientHeight;
+
+var desW = 640;
+var desH = 1136;
+if (winW / winH <= desW / desH) {
+    main.style.webkitTransform = "scale(" + winH / desH + ")";
+} else {
+    main.style.webkitTransform = "scale(" + winW / desW + ")";
+}
+
+[].forEach.call(oLis, function () {
+    var oLi = arguments[0];
+    oLi.index = arguments[1];
+    oLi.addEventListener("touchstart", start, false);
+    oLi.addEventListener("touchmove", move, false);
+    oLi.addEventListener("touchend", end, false);
+
+})
+
+function start(e) {
+    this.startTouch = e.changedTouches[0].pageY;
+}
+function move(e) {
+    this.flag = true
+    var moveTouch = e.changedTouches[0].pageY;
+    var pos = moveTouch - this.startTouch;
+    var index = this.index;
+    [].forEach.call(oLis,function(){
+        if(arguments[1]!=index){
+            arguments[0].style.display = "none";
+        }
+        arguments[0].className = "";
+        arguments[0].firstElementChild.id="";
+    });
+
+    if (pos > 0) {
+        this.prevSIndex = (index == 0 ? oLis.length - 1 : index - 1);
+        var duration = -winH+pos;
+
+    } else if (pos) {
+        this.prevSIndex = (index == oLis.length-1 ? 0 : index + 1);
+        var duration = winH+pos;
+    }
+    oLis[this.prevSIndex].style.display = "block";
+    oLis[this.prevSIndex].style.webkitTransform = "translate(0,"+duration+"px)";
+    oLis[this.prevSIndex].className="zIndex";
+    oLis[index].style.webkitTransform = "scale("+(1-Math.abs(pos)/winH*1/2)+") translate(0,"+pos+"px)";
+}
+function end(e) {
+    if(this.flag){
+        oLis[this.prevSIndex].style.webkitTransform = "translate(0,0)";
+        oLis[this.prevSIndex].style.webkitTransition = "0.7s";
+        oLis[this.prevSIndex].addEventListener("webkitTransitionEnd", function () {
+            this.style.webkitTransition = "";
+            this.firstElementChild.id="a"+this.index;
+
+        }, false)
+    }
+}
+
+ document.addEventListener("touchmove",function(){
+ },false);
+
